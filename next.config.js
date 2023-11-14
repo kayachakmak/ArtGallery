697c01f4a8291a0ed12f4ad4759496fc423906f3
Kaya Chakmak <kycakmak@gmail.com>
2023-11-14 16:02:31 +0100

    add resources

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["example-apis.vercel.app"],
+  },
   compiler: {
     styledComponents: true,
   },
-  reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
